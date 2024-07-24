bcc008ce81cd3a2435e861fa10d71e123d873b44
RomanBabych <151679619+RomanBabych@users.noreply.github.com>
2024-07-24 19:42:17 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div className={css.contactList}>
+      {filteredContacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
+      ))}
+    </div>
+  );
+}
---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Default to localStorage for web
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+    filters: filtersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
